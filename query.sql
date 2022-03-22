599f610b9f3a14939f8a63f3d596eb133e34ee08
MattOwings <mattalanowings@gmail.com>
2022-03-22 09:55:24 -0500

    sql updated, php added

--- query.sql
+++ query.sql
@@ -3,16 +3,25 @@ CREATE DATABASE instructor;
 USE instructor;
 
 CREATE TABLE register (
+    username VARCHAR(16) NOT NULL,
+    pass VARCHAR(40) NOT NULL,
+    email VARCHAR(40) NOT NULL,
+    masterkey INT(6) NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (email)
+
+);
+
+CREATE TABLE instructorProfile (
     firstname VARCHAR(30),
     middlename VARCHAR(30),
     lastname VARCHAR(30) NOT NULL,
+    birthday VARCHAR(30),
     gender VARCHAR(12) NOT NULL,
     ID INT(6) NOT NULL AUTO_INCREMENT,
-    pass VARCHAR(40) NOT NULL,
-
-    email VARCHAR(40) NOT NULL,
 
     PRIMARY KEY (ID)
+
 
 );
 
@@ -23,4 +32,12 @@ CREATE TABLE courses (
     courseClassroom VARCHAR(30) NOT NULL,
     courseSemester VARCHAR(30) NOT NULL,
     PRIMARY KEY (courseID)
+);
+
+CREATE TABLE students (
+    firstname VARCHAR(16) NOT NULL,
+    studentID VARCHAR(5) NOT NULL AUTO_INCREMENT,
+    finalGrade VARCHAR(5) NOT NULL,
+
+    PRIMARY KEY (studentID)
 );
